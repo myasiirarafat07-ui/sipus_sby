7dcf2783811b4008bdcd5f350b8b0c14a76a3dcb
Yasiir Arafat <myasiirarafat07@gmail.com>
2026-07-03 17:23:24 +0700

    Add PHP diagnostic test

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
+// Diagnostic test
+if (isset($_GET['test_php'])) {
+    echo "PHP is working! Version: " . phpversion();
+    exit;
+}
+
 // Forward Vercel requests to normal Laravel index.php
 require __DIR__ . '/../public/index.php';
